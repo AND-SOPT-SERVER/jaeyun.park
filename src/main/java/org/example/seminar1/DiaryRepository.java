108fa9c498f0af0ceeadc04fc19e19f530230cba
Parkjyun <jaeyun0507@naver.com>
2024-10-10 22:30:50 +0900

    [week1] 파일 저장 구조 생성

--- src/main/java/org/example/seminar1/DiaryRepository.java
+++ src/main/java/org/example/seminar1/DiaryRepository.java
@@ -1,60 +1,11 @@
 package org.example.seminar1;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
-public class DiaryRepository {
-
-    private final Map<Long, Diary> storage = new ConcurrentHashMap<>();
-    private final AtomicLong numbering = new AtomicLong();
-
-    List<Diary> findAll() {
-        return storage.values().stream()
-                .filter(diary -> !diary.getIsDeleted())
-                .toList();
-    }
-
-    void save(final Diary diary) {
-        final long id = numbering.addAndGet(1);//1추가하고 추가한 값 return
-        diary.setId(id);
-        storage.put(id, diary);
-    }
-
-    void patch(final long id, final String body) {
-        Diary diary = storage.get(id);
-        validateRetrieval(diary);
-        diary.setBody(body);
-        storage.put(diary.getId(), diary);
-    }
-
-    public void delete(long id) {
-        Diary diary = storage.get(id);
-        validateRetrieval(diary);
-        diary.setIsDeleted(true);
-        storage.put(diary.getId(), diary);
-    }
-
-    public void restore(long longId) {
-        Diary diary = storage.get(longId);
-        if (diary == null) {
-            throw new IllegalArgumentException("해당 id를 가진 Diary가 없습니다.");
-        }
-        if (!diary.getIsDeleted()) {
-            throw new IllegalStateException("해당 id를 가진 Diary는 삭제되지 않았습니다.");
-        }
-        diary.setIsDeleted(false);
-        storage.put(diary.getId(), diary);
-    }
-
-    private static void validateRetrieval(Diary diary) {
-        if (diary == null) {
-            throw new IllegalArgumentException("해당 id를 가진 Diary가 없습니다.");
-        }
-        if (diary.getIsDeleted()) {
-            throw new IllegalStateException("해당 id를 가진 Diary가 이미 삭제되었습니다.");
-        }
-    }
+public interface DiaryRepository {
+    void save(Diary diary);
+    void patch(long id, String body);
+    void delete(long id);
+    void restore(long id);
+    List<Diary> findAll();
 }
--- src/main/java/org/example/seminar1/DiaryService.java
+++ src/main/java/org/example/seminar1/DiaryService.java
@@ -1,28 +1,45 @@
 package org.example.seminar1;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DiaryService {
-    private final DiaryRepository diaryRepository = new DiaryRepository();
+    private Map<MemoryType, DiaryRepository > diaryRepositoryMap;
 
-    void writeDiary(final String body) {
+    DiaryService () {
+        diaryRepositoryMap = new HashMap<>();
+        diaryRepositoryMap.put(MemoryType.FILE, new FileDiaryRepository());
+        diaryRepositoryMap.put(MemoryType.MEMORY, new MemoryDiaryRepository());
+    }
+
+    void writeDiary(final String body, final MemoryType memoryType) {
         final Diary diary = new Diary(null, body, false);
-        diaryRepository.save(diary);
+        getDiaryRepository(memoryType).save(diary);
     }
 
-    List<Diary> getDiaryList() {
-        return diaryRepository.findAll();
+    List<Diary> getDiaryList(final MemoryType memoryType) {
+        return getDiaryRepository(memoryType).findAll();
     }
 
-    void patchDiary(final long id, final String body) {
-        diaryRepository.patch(id, body);
+    void patchDiary(final long id, final String body, final MemoryType memoryType) {
+        getDiaryRepository(memoryType).patch(id, body);
     }
 
-    void deleteDiary(final long id) {
-        diaryRepository.delete(id);
+    void deleteDiary(final long id, final MemoryType memoryType) {
+
+        getDiaryRepository(memoryType).delete(id);
     }
 
-    public void restoreDiary(long longId) {
-        diaryRepository.restore(longId);
+    public void restoreDiary(long longId, final MemoryType memoryType) {
+        getDiaryRepository(memoryType).restore(longId);
+    }
+
+    private DiaryRepository getDiaryRepository(MemoryType memoryType) {
+        DiaryRepository repository = diaryRepositoryMap.get(memoryType);
+        if (repository == null) {
+            throw new IllegalArgumentException("해당 MemoryType에 맞는 Repository가 없습니다: " + memoryType);
+        }
+        return repository;
     }
 }
