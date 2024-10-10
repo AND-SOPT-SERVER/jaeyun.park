c4a93265e7baa995857cbc77b30c8748f07d5320
Parkjyun <jaeyun0507@naver.com>
2024-10-10 21:49:46 +0900

    [week1] 수정, 삭제, 읽기, 변경 구현

---
+++ src/main/java/org/example/seminar1/DiaryService.java
@@ -0,0 +1,24 @@
+package org.example.seminar1;
+
+import java.util.List;
+
+public class DiaryService {
+    private final DiaryRepository diaryRepository = new DiaryRepository();
+
+    void writeDiary(final String body) {
+        final Diary diary = new Diary(null, body, false);
+        diaryRepository.save(diary);
+    }
+
+    List<Diary> getDiaryList() {
+        return diaryRepository.findAll();
+    }
+
+    void patchDiary(final long id, final String body) {
+        diaryRepository.patch(id, body);
+    }
+
+    void deleteDiary(final long id) {
+        diaryRepository.delete(id);
+    }
+}
