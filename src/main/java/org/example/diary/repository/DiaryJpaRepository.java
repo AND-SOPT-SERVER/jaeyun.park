021d47c64de624148098196a8b1a555873fffd91
Parkjyun <jaeyun0507@naver.com>
2024-10-26 02:56:29 +0900

    [week2] 필요없는 @Repository 삭제

--- src/main/java/org/example/diary/repository/DiaryJpaRepository.java
+++ src/main/java/org/example/diary/repository/DiaryJpaRepository.java
@@ -1,8 +1,6 @@
 package org.example.diary.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DiaryJpaRepository extends JpaRepository<DiaryJpaEntity, Long> {
 }
